af7b8dea43ca366dba3b3b8e003ced98cc6ef1c8
Laura López Gutiérrez <llopezgutie@uoc.edu>
2024-02-18 16:29:15 +0100

    Data from json ok

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PostList from "./PostList";
 
 const Home = () => {
-  const [posts, setPosts] = useState([
-    { title: "apojekpao", body: "jpaojfepo", tags: "er9u9" },
-    { title: "apojekpao", body: "jpaojfepo", tags: "er9u9" },
-    { title: "apojekpao", body: "jpaojfepo", tags: "er9u9" },
-  ]);
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3004/posts")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
 
   return (
-    <div className="home">
-      <PostList posts={posts} generictitle="all posts!!" />
+    <div className="home mt-10">
+      {posts && <PostList posts={posts} generictitle="all posts!!" />}
     </div>
   );
 };
--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -2,12 +2,20 @@ import React from "react";
 
 const PostList = ({ posts, generictitle }) => {
   return (
-    <div className="post-list">
-      <h2>{generictitle}</h2>
+    <div className="post-list ">
+      <div className="text-xl font-bold text-center mb-10">
+        <h2>{generictitle}</h2>
+      </div>
+
       {posts.map((post) => (
         <div className="post-preview" key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
+          <div className="border-2">
+            <h2 className="text-xl font-bold">{post.title}</h2>
+            <p>{post.body}</p>
+            <h6>{post.userId}</h6>
+            <h5 className="mx-10">{post.tags}, </h5>
+            <h6>{post.reactions}</h6>
+          </div>
         </div>
       ))}
     </div>
